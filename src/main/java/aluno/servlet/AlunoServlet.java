87fa1c07ef626cf3e0687f6e2f8d15bd8333a05b
deividgaliani <deividgaliani@gmail.com>
2018-07-08 01:24:49 -0300

    envio de formulário por parametro HTTP

--- src/main/java/aluno/servlet/AlunoServlet.java
+++ src/main/java/aluno/servlet/AlunoServlet.java
@@ -8,10 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns= {"/aluno", "/alunoController"})
-public class AlunoServlet extends HttpServlet{
+import aluno.domain.Aluno;
 
-	
+@WebServlet(urlPatterns= "/alunoController")
+public class AlunoServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//System.out.println("Metodo Get chamado");
@@ -21,14 +23,21 @@ public class AlunoServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("Metodo Post chamado");
-		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().print("Chamou pelo método POST!");
+		/*System.out.println("Metodo Post chamado");
+		resp.setCharacterEncoding("UTF-8");*/
+		
+		
+		String nome = req.getParameter("nome");
+		String idade = req.getParameter("idade");
+		Aluno aln = new Aluno();
+		aln.setNome(nome);
+		aln.setIdade(Integer.parseInt(idade));
+		resp.getWriter().print("Chamou pelo método POST... Nome : " + aln.getNome() + ", Idade: " + aln.getIdade());
 	}
 	
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().print("Método DELETE chamado");
+		//resp.setContentType("text/plain");
+		//resp.getWriter().print("index.html");
 	}
 }
